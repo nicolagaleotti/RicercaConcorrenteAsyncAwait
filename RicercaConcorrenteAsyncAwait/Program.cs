5d12c42417b08e155ae926bb3e90002e01382b20
Nicola Galeotti <niccio2802@outlook.it>
2021-04-22 11:18:15 +0200

    Add project files.

---
+++ RicercaConcorrenteAsyncAwait/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RicercaConcorrenteAsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeri = new int[100];
+            RiempiArray(ref numeri);
+
+            Console.WriteLine("Inserisci un numero da cercare:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(RicercaNumeroAsync(n1, numeri).Result);
+
+            Console.WriteLine("Inserisci un numero da cercare:");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(RicercaNumeroAsync(n2, numeri).Result);
+
+
+            Console.ReadLine();
+        }
+
+        private static async Task<bool> RicercaNumeroAsync(int n, int[] array)
+        {
+            bool trovato = false;
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (n == array[i])
+                    {
+                        trovato = true;
+                    }
+                }
+            });
+
+            return trovato;
+        }
+
+        private static void RiempiArray(ref int[] numeri)
+        {
+            Random r = new Random();
+            for (int n = 0; n < 100; n++)
+            {
+                numeri[n] = r.Next(0, 100);
+            }
+        }
+    }
+}
---
+++ RicercaConcorrenteAsyncAwait/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RicercaConcorrenteAsyncAwait")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RicercaConcorrenteAsyncAwait")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ab746553-51c0-4383-8dd8-b23572d46e4e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
